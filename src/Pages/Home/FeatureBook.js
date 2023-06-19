bfd47037cce2916564e7d7899ec089bf516aae17
Muhammad Haider Bukhari <85192296+haiderBukhari@users.noreply.github.com>
2023-06-19 15:34:08 +0500

    Update FeatureBook.js

--- src/Pages/Home/FeatureBook.js
+++ src/Pages/Home/FeatureBook.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { FetchProducts } from "../../Components/FetchData";
 import { ProductData } from "../../Components/ProductData";
 export const FeatureBook = () => {
+    let [loading, setloading] = useState(false);
     let [data, setdata] = useState([]);
     let link = `${process.env.REACT_APP_HOST}/featured_products`;
     FetchProducts({setdata , link});
@@ -10,8 +11,14 @@ export const FeatureBook = () => {
             <h1 data-aos="fade-up" className='text-2xl text-center font-semibold dark:text-slate-100 mb-10 mt-7 underline underline-offset-8'>Featured eBooks</h1>
             <div className='featured-book flex justify-center items-center flex-wrap'>
                 {
+                    !loading && <h1 className='text-2xl text-center font-semibold dark:text-slate-100  end m-0 p-0'>Loading ...</h1>
+                }
+                {
                     data.map((arr) => (
-                        <ProductData key={arr.id} arr={arr}/>
+                        <>
+                            <ProductData key={arr.id} arr={arr}/>
+                            {!loading && setloading(true)}
+                        </>
                     ))
                 }
             </div>
